7a3cac71053e863f962ec25d809de768d87be124
conuong <hothaipt123@gmail.com>
2024-11-11 12:15:11 +0700

    shoesshop

---
+++ assets/js/main.js
@@ -0,0 +1,196 @@
+// Auto hide header on scroll
+const headerNav = document.querySelector(".header-bottom");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if(lastScrollY < window.scrollY) {
+        headerNav.classList.add("hide")
+    } else {
+        headerNav.classList.remove("hide")
+    }
+    lastScrollY = window.scrollY;
+})
+
+// Close popup 
+const body = document.querySelector("body");
+let     modalContainer = document.querySelectorAll('.modal');
+let modalBox = document.querySelectorAll('.mdl-cnt');
+let formLogSign = document.querySelector('.forms');
+
+// Click vùng ngoài sẽ tắt Popup
+modalContainer.forEach(item => {
+    item.addEventListener('click', closeModal);
+});
+
+modalBox.forEach(item => {
+    item.addEventListener('click', function (event) {
+        event.stopPropagation();
+    })
+});
+
+function closeModal() {
+    modalContainer.forEach(item => {
+        item.classList.remove('open');
+    });
+    console.log(modalContainer)
+    body.style.overflow = "auto";
+}
+
+function increasingNumber(e) {
+    let qty = e.parentNode.querySelector('.input-qty');
+    if (parseInt(qty.value) < qty.max) {
+        qty.value = parseInt(qty.value) + 1;
+    } else {
+        qty.value = qty.max;
+    }
+}
+
+function decreasingNumber(e) {
+    let qty = e.parentNode.querySelector('.input-qty');
+    if (qty.value > qty.min) {
+        qty.value = parseInt(qty.value) - 1;
+    } else {
+        qty.value = qty.min;
+    }
+}
+
+// Open Search Advanced
+document.querySelector(".filter-btn").addEventListener("click",(e) => {
+    e.preventDefault(); 
+    document.querySelector(".advanced-search").classList.toggle("open");
+    document.getElementById("home-service").scrollIntoView();
+})
+
+document.querySelector(".form-search-input").addEventListener("click",(e) => {
+    e.preventDefault();
+    document.getElementById("home-service").scrollIntoView();
+})
+
+function closeSearchAdvanced() {
+    document.querySelector(".advanced-search").classList.toggle("open");
+}
+
+function openSearchMb() {
+    document.querySelector(".header-middle-left").style.display = "none";
+    document.querySelector(".header-middle-center").style.display = "block";
+    document.querySelector(".header-middle-right-item.close").style.display = "block";
+    let liItem = document.querySelectorAll(".header-middle-right-item.open");
+    for(let i = 0; i < liItem.length; i++) {
+        liItem[i].style.setProperty("display", "none", "important")
+    }
+}
+
+//Close Search Mobile 
+function closeSearchMb() {
+    document.querySelector(".header-middle-left").style.display = "block";
+    document.querySelector(".header-middle-center").style.display = "none";
+    document.querySelector(".header-middle-right-item.close").style.display = "none";
+    let liItem = document.querySelectorAll(".header-middle-right-item.open");
+    for(let i = 0; i < liItem.length; i++) {
+        liItem[i].style.setProperty("display", "block", "important")
+    }
+}
+
+
+// imgame slider
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+}
+
+        
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+// find product
+var productAll = JSON.parse(localStorage.getItem('products')).filter(item => item.status == 1);
+function searchProducts(mode) {
+    let valeSearchInput = document.querySelector('.form-search-input').value;
+    let valueCategory = document.getElementById("advanced-search-category-select").value;
+    let sizeS=document.getElementById("size").value;
+    let minPrice = document.getElementById("min-price").value;
+    let maxPrice = document.getElementById("max-price").value;
+    if(parseInt(minPrice) > parseInt(maxPrice) && minPrice != "" && maxPrice != "") {
+        alert("Giá đã nhập sai !");
+    }
+
+    if (parseInt(sizeS) < 30 || parseInt(sizeS) > 45) {
+        alert("Size không hợp lệ! Vui lòng chọn từ 30 đến 45.");
+    }
+
+    let result = valueCategory == "Tất cả" ? productAll : productAll.filter((item) => {
+        return item.category == valueCategory;
+    });
+
+    result = valeSearchInput == "" ? result : result.filter(item => {
+        return item.title.toString().toUpperCase().includes(valeSearchInput.toString().toUpperCase());
+    })
+
+    if(minPrice == "" && maxPrice != "") {
+        result = result.filter((item) => item.price <= maxPrice);
+    } else if (minPrice != "" && maxPrice == "") {
+        result = result.filter((item) => item.price >= minPrice);
+    } else if(minPrice != "" && maxPrice != "") {
+        result = result.filter((item) => item.price <= maxPrice && item.price >= minPrice);
+    }
+
+    if (sizeS !== "") {
+        if (sizeS >= 36 && sizeS <= 45) {
+            result = result.filter((item) => item.size == sizeS);
+        }
+    }
+
+    document.getElementById("home-service").scrollIntoView();
+    switch (mode){
+        case 0:
+            result = JSON.parse(localStorage.getItem('products'));;
+            document.querySelector('.form-search-input').value = "";
+            document.getElementById("advanced-search-category-select").value = "Tất cả";
+            document.getElementById("size").value = "";
+            document.getElementById("min-price").value = "";
+            document.getElementById("max-price").value = "";
+            break;
+        case 1:
+            result.sort((a,b) => a.price - b.price)
+            break;
+        case 2:
+            result.sort((a,b) => b.price - a.price)
+            break;
+    }
+    showHomeProduct(result)
+}
+
+
+
